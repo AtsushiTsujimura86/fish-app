9171dc7e3c1a0223310377115d9c9d0b4735de5d
Atsushi Tsujimura <a.tnba1386@gmail.com>
2025-02-24 21:08:25 +0900

    update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const API_URL = window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://fish-app-backend-ten.vercel.app/api";
+    : "https://fish-creater-api.vercel.app/api";
 
 export default API_URL;
